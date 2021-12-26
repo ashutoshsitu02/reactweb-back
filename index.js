970746ab792db23b15c01a539c0e99dcefc32e49
ashutoshsitu02 <ashutoshsitu20@gmail.com>
2021-12-27 00:51:04 +0530

    make it better world

--- index.js
+++ index.js
@@ -3,33 +3,67 @@ const express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const PORT =3001;
+const PORT =3000;
 
 
-var mysqlConnection = mysql.createConnection({
+// var mysqlConnection = mysql.createConnection({
+//     host: 'us-cdbr-east-05.cleardb.net',
+//     user: 'bb0a4dc79d027b',
+//     password: '2c7938d3',
+//     database: 'heroku_d6cb274657ee475',
+//     multipleStatements: true
+// });
+
+var db_config = {
     host: 'us-cdbr-east-05.cleardb.net',
-    user: 'b0d640e1aa24d8',
-    password: 'b4cc4c98',
-    database: 'heroku_fd439ebdc1be0a1',
+    user: 'bb0a4dc79d027b',
+    password: '2c7938d3',
+    database: 'heroku_d6cb274657ee475',
     multipleStatements: true
-});
+};
 
+
+// mysqlConnection.connect((err) => {
+//     if (!err)
+//         console.log('DB connection succeded.');
+        
+//     else{
+//         console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+//         setTimeout(handleDisconnect, 2000);
+//     }
+// });
+
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err) { 
+        if(err) {
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+        else
+        console.log('DB connection succeded.');
+    });
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}));
 
-
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('DB connection succeded.');
-    else
-        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
-});
-
 //get
-app.get('/api/get', (req, res) => {
-    mysqlConnection.query('SELECT * FROM factory', (err, rows, fields) => {
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM factory', (err, rows, fields) => {
         if (!err)
             res.send(rows);
         else
@@ -38,7 +72,7 @@ app.get('/api/get', (req, res) => {
 });
 
 //Insert
-app.post("/api/insert",(req,res)=>{
+app.post("/insert",(req,res)=>{
 
     const Name =req.body.Name
     const Address = req.body.Address
@@ -46,14 +80,14 @@ app.post("/api/insert",(req,res)=>{
     const Email=req.body.Email
 
     const sqlInsert="INSERT INTO factory(Factory_name,location,email) VALUE (?,?,?)";
-    mysqlConnection.query(sqlInsert,[ Name, Address,/* Phonenumber,*/ Email]),(err,result)=>{
+    connection.query(sqlInsert,[ Name, Address,/* Phonenumber,*/ Email]),(err,result)=>{
         console.log(result)
     }
 });
 
 //Delete an factory
-app.delete('/api/delete/:id', (req, res) => {
-    mysqlConnection.query('DELETE FROM factory WHERE Factory_id = ?', [req.params.id], (err, rows, fields) => {
+app.delete('/delete/:id', (req, res) => {
+    connection.query('DELETE FROM factory WHERE Factory_id = ?', [req.params.id], (err, rows, fields) => {
         if (err)
         console.log(err);
     })
@@ -61,10 +95,10 @@ app.delete('/api/delete/:id', (req, res) => {
 
 //update factory
 
-app.put('/api/update',(req,res)=>{
+app.put('/update',(req,res)=>{
     const Name=req.body.Name;
     const id = req.body.id;
-    mysqlConnection.query('UPDATE factory SET Factory_name = ? WHERE Factory_id=?',[Name,id],(err,result)=>{
+    connection.query('UPDATE factory SET Factory_name = ? WHERE Factory_id=?',[Name,id],(err,result)=>{
         if(err){
             console.log(err)
         }else{
